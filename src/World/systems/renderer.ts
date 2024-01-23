1681a9beba48a602f232760072977ef4a1f4161a
Winston Cooke <git@winstoncooke.com>
2024-01-23 10:29:19 -0600

    update dependencies

--- src/World/systems/renderer.ts
+++ src/World/systems/renderer.ts
@@ -2,7 +2,6 @@ import { WebGLRenderer } from 'three';
 
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: true });
-  renderer.physicallyCorrectLights = true;
   return renderer;
 }
 
